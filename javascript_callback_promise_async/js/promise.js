e39d34c8073d3d2082a40155475b848d2d60ddf3
Dibesh Sharma <dibesh.sharma@kingliving.com.au>
2023-01-07 00:26:34 +1100

    commit

--- javascript_callback_promise_async/js/promise.js
+++ javascript_callback_promise_async/js/promise.js
@@ -54,4 +54,48 @@ let myPromise = new Promise(function(myResolve, myReject) {
   }
 
 
+  const posts = [
+    {title: 'Post One', body: 'This is post one'},
+    {title: 'Post Tow', body: 'This is post tow'}
+]
+
+function getPosts(){
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output
+    }, 1000)
+}
+
+function createPost(post){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = false;
+
+            if(!error){
+                resolve();
+            }
+            else {
+                reject('Error: Something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+createPost({ title: 'Post There', body: 'This is post three'})
+    .then(getPosts)
+    .catch(err => console.log(err));
+
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, rejetct) => setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+
+
 
